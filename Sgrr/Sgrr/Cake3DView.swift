41a02bc4cb569d0b4692effac64b955d04611204
Dora Choi <135581648+dorachoii@users.noreply.github.com>
2024-07-25 21:32:08 +0900

    feat: 3D model upload
    케이크 모델 올리기까지 성공

--- Sgrr/Sgrr/Cake3DView.swift
+++ Sgrr/Sgrr/Cake3DView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 import ARKit
 import RealityKit
 
+let cakeMaterial = SimpleMaterial(color: .systemPink, isMetallic: false)
+
 struct Cake3DView: View {
     var body: some View {
         ARViewContainer().edgesIgnoringSafeArea(.all)
@@ -16,11 +18,11 @@ struct Cake3DView: View {
 }
 
 struct ARViewContainer: UIViewRepresentable {
-
+    
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero, cameraMode: .nonAR, automaticallyConfigureSession: true)
-
+        
         let cakeTrayModel = try! ModelEntity.loadModel(named: "cakeTray")
         cakeTrayModel.scale = SIMD3(x: 0.2, y: 0.2, z: 0.2)
         cakeTrayModel.generateCollisionShapes(recursive: true)
@@ -30,17 +32,22 @@ struct ARViewContainer: UIViewRepresentable {
         cakeModel.generateCollisionShapes(recursive: true)
         
         // Create a cube model
-//        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-//        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-//        let model = ModelEntity(mesh: mesh, materials: [material])
+        //        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
+        //        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
+        //        let model = ModelEntity(mesh: mesh, materials: [material])
         
         // Create world anchor at the origin
         let anchor = AnchorEntity(world: [0, 0, 0])
         anchor.addChild(cakeTrayModel)
         anchor.addChild(cakeModel)
-
+        
         // Add the anchor to the scene
         arView.scene.anchors.append(anchor)
+        
+        // 시간흐르게 하는 코드 확인 필요
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            cakeModel.model?.materials = [cakeMaterial]
+        }
         
         return arView
     }
