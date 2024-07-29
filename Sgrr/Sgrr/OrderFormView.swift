d32cebcacdb8c7f26d19ac5c7cc667909c42849d
KIM SEOWOO <sc204016@gmail.com>
2024-07-29 15:13:03 +0900

    feat/#8 orderformview 파일 생성

---
+++ Sgrr/Sgrr/OrderFormView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderFormView.swift
+//  Sgrr
+//
+//  Created by KIM SEOWOO on 7/26/24.
+//
+
+import SwiftUI
+
+struct OrderFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderFormView()
+}
