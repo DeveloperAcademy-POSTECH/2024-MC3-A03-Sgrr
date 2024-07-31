22c447f9914c7d991b0a2552cf1a26467f76a1b9
Dora Choi <135581648+dorachoii@users.noreply.github.com>
2024-08-01 00:16:32 +0900

    test
    test

--- Sgrr/Sgrr/3D/testView.swift
+++ Sgrr/Sgrr/3D/testView.swift
@@ -14,7 +14,6 @@ struct testView: View {
         VStack {
             
             Cake3DView(cakeImage: $cakeImage)
-            Divider()
             CakeCanvasView(cakeImage: $cakeImage)
         }
        
